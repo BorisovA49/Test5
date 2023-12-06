f475be86a258356b25c958f8096cd32d119f4186
BorisovA49 <ewqazxc123123@gmail.com>
2023-12-06 03:36:14 +0300

    ver4

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,40 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // создали объект
-        try {
+        Scanner sc = new Scanner(System.in); // создали объектв
             int year = sc.nextInt(); // год начала работы
-            convenientFunction(year);
-        } catch (Exception exception) {
+            exceptions(year);
+    }
+    private static float exceptions(int year) {
+        float a = 0; // еще одна не нужная функция
+        if (year >= 2002 && year < 2022) {
+            a = convenientFunction(year);
+        }
+        else {
             System.out.println("throws exception...");
         }
-        //System.out.println("(─‿‿─)");//Я думаю что именно для этой зазписи вы меня  попросили ввести искдючения в виде try и catch
+        return a;
     }
-
-    private static void convenientFunction(int year) {
-        final double[] initialSum = Constants.MOEX_RATE; // выдем значение из класса Constants
-        final double[] inflation = Constants.INFLATION_RATE; // выдем значение из класса Constants
-        float seizurePercentage = 0; // процент изъятия в процентах
+        private static float convenientFunction(int year) {
+        float seizurePercentage = 0; // процент изъятия в процентах ключ
         while (true) {
-            double initialCapital = initialSum[year - 2002];
-            seizurePercentage += 0.5;
-            double cash = initialSum[year - 2002] * seizurePercentage * 0.01; // процент изъятия в деньгах (бумажных)
+            double initialCapital = Constants.MOEX_RATE[year - 2002]; // начальный капитал нельзя избваться, переменная будет изменятсья
+            seizurePercentage += 0.5; // Прибавка к капиталу -
+            double cash = Constants.MOEX_RATE[year - 2002] * seizurePercentage * 0.01; // процент изъятия в деньгах (бумажных)
             for (int y = 0; y < 2022 - year; y++) {
                 initialCapital -= cash;
-                initialCapital = initialCapital * (initialSum[year - 2002 + y + 1] / initialSum[year - 2002 + y]);
-                cash = cash * (100 + inflation[year - 2002 + y]) / 100;
+                initialCapital = initialCapital * (Constants.MOEX_RATE[year - 2002 + y + 1] / Constants.MOEX_RATE[year - 2002 + y]);
+                cash = cash * (100 + Constants.INFLATION_RATE[year - 2002 + y]) / 100;
             }
             if (initialCapital < 0) {
                 System.out.println(seizurePercentage - 0.5);
-
-                break;
-
-            }
-            if (seizurePercentage > 100) {
-                System.out.println(seizurePercentage);
                 break;
             }
         }
-    }
+            return seizurePercentage;
+        }
 }
 
 
