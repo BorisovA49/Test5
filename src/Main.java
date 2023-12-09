6f728ebfb6d256cedc868c250ddefdb9930902cf
BorisovA49 <ewqazxc123123@gmail.com>
2023-12-09 18:54:51 +0300

    222

--- src/Main.java
+++ src/Main.java
@@ -5,35 +5,33 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // создали объектв ss
         int year = sc.nextInt(); // год начала работы
-        exceptions(year);
-    }
-    private static float exceptions(int year) {
-        float a = 0; // еще одна не нужная функция
-        if (year >= 2002 && year < 2022) {
-            a = convenientFunction(year);
+        if (year <= 2001 || year >= 2022) {
+            throw new ArrayIndexOutOfBoundsException("throws Exception…");
         }
-        else {
-            System.out.println("throws exception...");
-        }
-        return a;
+        convenientFunction(year);
     }
-    private static float convenientFunction(int year) {
-        float seizurePercentage = 0; // процент изъятия в процентах ключ
+
+    private static void convenientFunction(int year) {
+
+        float withdrawalPercentage = 0; // процент изъятия в процентах ключ
         while (true) {
             double initialCapital = Constants.MOEX_RATE[year - 2002]; // начальный капитал нельзя избваться, переменная будет изменятсья
-            seizurePercentage += 0.5; // Прибавка к капиталу -
-            double cash = Constants.MOEX_RATE[year - 2002] * seizurePercentage * 0.01; // процент изъятия в деньгах (бумажных)
-            for (int y = 0; y < 2022 - year; y++) {
-                initialCapital -= cash;
-                initialCapital = initialCapital * (Constants.MOEX_RATE[year - 2002 + y + 1] / Constants.MOEX_RATE[year - 2002 + y]);
-                cash = cash * (100 + Constants.INFLATION_RATE[year - 2002 + y]) / 100;
-            }
-            if (initialCapital < 0) {
-                System.out.println(seizurePercentage - 0.5);
+            withdrawalPercentage += 0.5; // Прибавка к капиталу -
+            double withdrawal = Constants.MOEX_RATE[year - 2002] * withdrawalPercentage * 0.01; // процент изъятия в деньгах (бумажных)
+            if (veryConvenientFunction(year, initialCapital, withdrawal) < 0) {
+                System.out.println(withdrawalPercentage - 0.5);
                 break;
             }
         }
-        return seizurePercentage;
+    }
+
+    private static double veryConvenientFunction(int year, double initialCapital, double withdrawal) {
+        for (int y = 0; y < 2022 - year; y++) {
+            initialCapital -= withdrawal;
+            initialCapital *= (Constants.MOEX_RATE[year - 2002 + y + 1] / Constants.MOEX_RATE[year - 2002 + y]);
+            withdrawal *= (100 + Constants.INFLATION_RATE[year - 2002 + y]) / 100;
+        }
+        return initialCapital;
     }
 }
 
